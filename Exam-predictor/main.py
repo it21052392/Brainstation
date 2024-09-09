a6d16653c7ef059c03c87d100aaa941dbe08610a
it21052392 <it21052392@my.sliit.lk>
2024-09-09 20:13:22 +0530

    feat: Exam predictor

--- Exam-predictor/main.py
+++ Exam-predictor/main.py
@@ -79,7 +79,7 @@
 
 # if __name__ == "__main__":
 #     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=9002)
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 from fastapi import FastAPI
 from pydantic import BaseModel
