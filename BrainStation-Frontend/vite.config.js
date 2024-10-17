223ab43e14f9d160c9d3dab767f6f55c0a35a3a9
it21052392 <it21052392@my.sliit.lk>
2024-10-18 04:18:33 +0530

    Feat : docker

--- BrainStation-Frontend/vite.config.js
+++ BrainStation-Frontend/vite.config.js
@@ -1,22 +1,29 @@
 import react from "@vitejs/plugin-react";
-import { default as path } from "path";
 import { defineConfig } from "vite";
+import fs from "fs";
+import path from "path";
 
-export default () => {
-  return defineConfig({
-    plugins: [react()],
-    base: "./",
-    resolve: {
-      alias: {
-        "@/": `${path.resolve(__dirname, "src")}/`
-      }
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        define: {
-          global: "globalThis"
-        }
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis"
       }
     }
-  });
-};
+  },
+  server: {
+    https: {
+      key: fs.readFileSync("/app/localhost.key"),
+      cert: fs.readFileSync("/app/localhost.crt"),
+    },
+    host: '0.0.0.0',
+    port: 5173
+  }
+});
