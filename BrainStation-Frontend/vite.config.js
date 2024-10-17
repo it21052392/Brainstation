588af26d8aa8a7380f2592c624e4f3efe4e76ad9
it21052392 <it21052392@my.sliit.lk>
2024-10-18 04:13:36 +0530

    fix

--- BrainStation-Frontend/vite.config.js
+++ BrainStation-Frontend/vite.config.js
@@ -1,28 +1,22 @@
 import react from "@vitejs/plugin-react";
+import { default as path } from "path";
 import { defineConfig } from "vite";
-import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "./", // Base public path
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Alias '@/' to 'src' directory
+export default () => {
+  return defineConfig({
+    plugins: [react()],
+    base: "./",
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`
+      }
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis", // Defining global for compatibility
-      },
-    },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync('/app/localhost.key'),
-      cert: fs.readFileSync('/app/localhost.crt'),
-    },
-    host: '0.0.0.0',  // Expose the server to external devices
-    port: 5173,       // Default port for Vite
-  },
-});
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: "globalThis"
+        }
+      }
+    }
+  });
+};
