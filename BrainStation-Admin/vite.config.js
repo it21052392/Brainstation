f7b63b1ad38cc26b92dff2cc4425c48a336dd227
it21052392 <it21052392@my.sliit.lk>
2024-10-28 19:33:43 +0530

    Refactor: Update Dockerfile to expose port 9009

--- BrainStation-Admin/vite.config.js
+++ BrainStation-Admin/vite.config.js
@@ -1,22 +1,26 @@
 import react from "@vitejs/plugin-react";
-import { default as path } from "path";
+import path from "path";
 import { defineConfig } from "vite";
 
-export default () => {
-  return defineConfig({
-    plugins: [react()],
-    base: "./",
-    resolve: {
-      alias: {
-        "@/": `${path.resolve(__dirname, "src")}/`
-      }
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        define: {
-          global: "globalThis"
-        }
+export default defineConfig({
+  plugins: [
+    react() 
+  ],
+  base: "./", 
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/` 
+    }
+  },
+  server: {
+    port: 9009, 
+    host: true  
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis" 
       }
     }
-  });
-};
+  }
+});
